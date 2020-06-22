b1b89fd516e334a45ce334b5fcbfe9625621543d
andrewleverette <andrew.leverette@decisionsource.com>
2020-06-22 16:41:08 -0500

    Add read from file example

--- src/bin/csv_read_file.rs
+++ src/bin/csv_read_file.rs
@@ -1,20 +1,19 @@
 use std::error::Error;
-use std::io;
 
 use csv;
 
-/// Reads data from `stdin` into a reader and prints all records.
+/// Reads data from a file into a reader and prints all records.
 /// 
 /// # Error
 /// 
 /// If an error occurs, the error is returned to `main`.
-fn read_from_stdin() -> Result<(), Box<dyn Error>> {
-    // Creates a new csv `Reader` from `stdin`
-    let mut reader = csv::Reader::from_reader(io::stdin());
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+    // Creates a new csv `Reader` from a file
+    let mut reader = csv::Reader::from_path(path)?;
 
+    // Retrieve and print header record
     let headers = reader.headers()?;
-
-    println!("Headers: {:?}", headers);
+    println!("{:?}", headers);
 
     // `.records` return an iterator of the internal 
     // record structure
@@ -29,7 +28,7 @@ fn read_from_stdin() -> Result<(), Box<dyn Error>> {
 
 fn main() {
     // If an error occurs print error
-    if let Err(e) = read_from_stdin() {
+    if let Err(e) = read_from_file("./data/Customers.csv") {
         eprintln!("{}", e);
     }
 }
