0a1a6b5e865bb80e1578c555d868b4ea06c79d78
andrewleverette <andrew.leverette@decisionsource.com>
2020-06-24 10:17:33 -0500

    Add logic to write records

--- src/bin/csv_aggregation.rs
+++ src/bin/csv_aggregation.rs
@@ -2,10 +2,7 @@ use std::error::Error;
 use std::fmt;
 use std::process;
 
-use csv::{
-    Reader,
-    StringRecord
-};
+use csv::{Reader, StringRecord, Writer};
 
 #[derive(Debug)]
 struct IndexError(String);
@@ -21,32 +18,35 @@ impl Error for IndexError {}
 #[derive(Debug)]
 struct DataSet {
     headers: StringRecord,
-    records: Vec<StringRecord>
+    records: Vec<StringRecord>,
 }
 
 impl DataSet {
     fn new(headers: StringRecord, records: Vec<StringRecord>) -> Self {
-        DataSet {
-            headers,
-            records
-        }
+        DataSet { headers, records }
     }
 
     fn key_index(&self, key: &str) -> Result<usize, Box<dyn Error>> {
         match self.headers.iter().position(|field| field == key) {
             Some(index) => Ok(index),
-            None => Err(Box::new(IndexError(format!("Column '{}' does not exist.", key))))
+            None => Err(Box::new(IndexError(format!(
+                "Column '{}' does not exist.",
+                key
+            )))),
         }
     }
-    
+
     fn sort_by_index(&mut self, index: usize) -> Result<(), Box<dyn Error>> {
         if index >= self.headers.len() {
-            return Err(Box::new(IndexError(format!("Index '{}' out of bounds", index))));
+            return Err(Box::new(IndexError(format!(
+                "Index '{}' out of bounds",
+                index
+            ))));
         } else {
             self.records.sort_by(|a, b| a[index].cmp(&b[index]));
             Ok(())
         }
-    }    
+    }
 }
 
 trait Aggregate {
@@ -58,11 +58,11 @@ impl Aggregate for DataSet {
         let left_index = self.key_index(key)?;
         let right_index = right.key_index(key)?;
 
-        
         let headers = StringRecord::from(
-            self.headers.iter()
+            self.headers
+                .iter()
                 .chain(right.headers.iter())
-                .collect::<Vec<&str>>()
+                .collect::<Vec<&str>>(),
         );
 
         let mut records = vec![];
@@ -80,19 +80,23 @@ impl Aggregate for DataSet {
         while left_cursor < self.records.len() && right_cursor < right.records.len() {
             if self.records[left_cursor][left_index] == right.records[right_cursor][right_index] {
                 let record = StringRecord::from(
-                    self.records[left_cursor].iter()
+                    self.records[left_cursor]
+                        .iter()
                         .chain(right.records[right_cursor].iter())
-                        .collect::<Vec<&str>>()
+                        .collect::<Vec<&str>>(),
                 );
 
                 records.push(record);
 
                 let mut k = right_cursor + 1;
-                while k < right.records.len() && self.records[left_cursor][left_index] == right.records[k][right_index]{
+                while k < right.records.len()
+                    && self.records[left_cursor][left_index] == right.records[k][right_index]
+                {
                     let record = StringRecord::from(
-                        self.records[left_cursor].iter()
+                        self.records[left_cursor]
+                            .iter()
                             .chain(right.records[right_cursor].iter())
-                            .collect::<Vec<&str>>()
+                            .collect::<Vec<&str>>(),
                     );
 
                     records.push(record);
@@ -101,7 +105,9 @@ impl Aggregate for DataSet {
                 }
                 left_cursor += 1;
                 continue;
-            } else if self.records[left_cursor][left_index] < right.records[right_cursor][right_index] {
+            } else if self.records[left_cursor][left_index]
+                < right.records[right_cursor][right_index]
+            {
                 left_cursor += 1;
             } else {
                 right_cursor += 1;
@@ -117,12 +123,23 @@ fn read_from_file(path: &str) -> Result<DataSet, Box<dyn Error>> {
 
     let headers = reader.headers()?.clone();
 
-    let records = reader.records().collect::<Result<Vec<StringRecord>, csv::Error>>()?;
+    let records = reader
+        .records()
+        .collect::<Result<Vec<StringRecord>, csv::Error>>()?;
 
-    Ok(DataSet {
-        headers,
-        records
-    })
+    Ok(DataSet { headers, records })
+}
+
+fn write_to_file(data: DataSet, path: &str) -> Result<(), Box<dyn Error>> {
+    let mut writer = Writer::from_path(path)?;
+
+    writer.write_record(data.headers.iter())?;
+
+    for record in data.records {
+        writer.write_record(record.iter())?;
+    }
+
+    Ok(())
 }
 
 fn main() {
@@ -146,7 +163,14 @@ fn main() {
         Ok(data) => data,
         Err(e) => {
             eprintln!("{}", e);
-            process::exit(1);    
+            process::exit(1);
         }
     };
+
+    if let Err(e) = write_to_file(result, "./data/JoinedRecords.csv") {
+        eprintln!("{}", e);
+        process::exit(1);
+    } else {
+        println!("Inner Join Complete");
+    }
 }
